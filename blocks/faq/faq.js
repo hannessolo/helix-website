f672c3300ba6fca9850de6ac6c33d3fea6024e36
Hannes Hertach <hanneshertach@gmail.com>
2022-09-09 13:15:04 +0200

    add css to faq

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -1,3 +1,7 @@
 export default function decorate(block) {
   block.classList.add('faq');
+  const children = Array.from(block.children);
+
+  children.forEach((child) => child.children[0]?.classList.add('question'));
+  children.forEach((child) => child.children[1]?.classList.add('answer'));
 }
